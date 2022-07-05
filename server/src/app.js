ac5d699fea05f720e15e4548647502e9d6427810
José Sampaio <jose@pippa.com.br>
2022-07-05 13:58:15 -0300

    CORS Middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
+const whitelist = ['http://localhost:3000']
 
-app.use(planetsRouter);
-
+app.use(cors({
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+          callback(null, true)
+        } else {
+          callback(new Error('Not allowed by CORS'))
+        }
+      }
+}));
 app.use(express.json());
+
+//ROUTES
+app.use(planetsRouter);
 
 module.exports = app;
